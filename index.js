7e56cb63eb739496c4805fb2deee7de9c9b9553d
Gabriel Mercante <gabriel.mercante@pontaltech.com.br>
2022-04-01 17:03:37 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import smpp from 'smpp';
+
+const session = smpp.connect('localhost', 9400);
+let smppConnected = false;
+
+session.on('connect', () => {
+    session.bind_transceiver({
+        system_id: 'teste',
+        password: 'q1w2e3',
+        system_type: '',
+        interface_version: 52,
+    }, (pdu) => {
+        if (pdu.command_status == 0) {
+            smppConnected = true;
+            console.log("outsession bind completed ok, status 0");
+        }
+    })
+});
+
+session.on('pdu', function(pdu){
+    console.log({smppPdu:pdu}, Boolean(pdu.queue_id));
+});
+
+session.on('close', () => {
+    console.log('smpp disconnected');
+    smppConnected = false;
+    console.log('smpp reconnecting');
+    session.connect('http://localhost', 9400);
+});
+
+session.on('error', (error) => {
+    console.log('smpp error', error)
+});
